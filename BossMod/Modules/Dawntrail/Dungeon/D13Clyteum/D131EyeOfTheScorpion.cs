522b29e714f5fadca23e37b66d2f2014596b52ff
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2026-05-01 14:21:51 -0400

    scorp a dorp

--- BossMod/Modules/Dawntrail/Dungeon/D13Clyteum/D131EyeOfTheScorpion.cs
+++ BossMod/Modules/Dawntrail/Dungeon/D13Clyteum/D131EyeOfTheScorpion.cs
@@ -9,51 +9,48 @@ public enum OID : uint
 
 public enum AID : uint
 {
-    _AutoAttack_ = 50170, // 4EB6/4DD2->4C09/4C0A, no cast, single-target
-    _AutoAttack_1 = 50110, // Boss->player, no cast, single-target
-    _Weaponskill_EyesOnMe = 48896, // Boss->self, 5.0s cast, range 35 circle
-    _Weaponskill_PetrifyingBeam = 50176, // Boss->self, 8.0+0.5s cast, single-target
-    _Weaponskill_PetrifyingBeam1 = 50178, // Helper->self, 8.5s cast, range 70 100-degree cone
-    _Weaponskill_PetrifyingBeam2 = 50175, // Boss->self, 8.0+0.5s cast, single-target
-    _Weaponskill_PetrifyingBeam3 = 50177, // Helper->self, 8.5s cast, range 70 100-degree cone
-    _Weaponskill_MotionScanner = 48893, // Boss->self, 4.0s cast, single-target
-    _Weaponskill_Launch = 48895, // Helper->player, no cast, single-target
-    _Weaponskill_BallisticMissile = 48897, // Boss->self, no cast, single-target
-    _Weaponskill_PenetratorMissile = 48901, // Helper->players, 5.0s cast, range 6 circle
-    _Weaponskill_SurfaceMissile = 48898, // Helper->location, 3.0s cast, range 5 circle
-    _Weaponskill_AntiPersonnelMissile = 48899, // Helper->player, 5.0s cast, range 6 circle
+    AutoAttack = 50110, // Boss->player, no cast, single-target
+    EyesOnMe = 48896, // Boss->self, 5.0s cast, range 35 circle
+    PetrifyingBeamCast1 = 50175, // Boss->self, 8.0+0.5s cast, single-target
+    PetrifyingBeamCast2 = 50176, // Boss->self, 8.0+0.5s cast, single-target
+    PetrifyingBeam1 = 50177, // Helper->self, 8.5s cast, range 70 100-degree cone
+    PetrifyingBeam2 = 50178, // Helper->self, 8.5s cast, range 70 100-degree cone
+    MotionScanner = 48893, // Boss->self, 4.0s cast, single-target
+    Launch = 48895, // Helper->player, no cast, single-target
+    BallisticMissile = 48897, // Boss->self, no cast, single-target
+    PenetratorMissile = 48901, // Helper->players, 5.0s cast, range 6 circle
+    SurfaceMissile = 48898, // Helper->location, 3.0s cast, range 5 circle
+    AntiPersonnelMissile = 48899, // Helper->player, 5.0s cast, range 6 circle
 }
 
 public enum SID : uint
 {
-    _Gen_DirectionalDisregard = 3808, // none->Boss, extra=0x0
-    _Gen_MotionTracker = 5191, // none->player, extra=0x0
-    _Gen_VulnerabilityUp = 1789, // Helper->player, extra=0x1/0x2
+    MotionTracker = 5191, // none->player, extra=0x0
 }
 
 public enum IconID : uint
 {
-    _Gen_Icon_m0489trg_a0c = 136, // player->self
-    _Gen_Icon_com_share0c = 62, // player->self
-    _Gen_Icon_target_ae_s5f = 139, // player->self
+    Checkmark = 136, // player->self
+    Stack = 62, // player->self
+    Spread = 139, // player->self
 }
 
-class EyesOnMe(BossModule module) : Components.RaidwideCast(module, AID._Weaponskill_EyesOnMe);
-class PetrifyingBeam(BossModule module) : Components.GroupedAOEs(module, [AID._Weaponskill_PetrifyingBeam1, AID._Weaponskill_PetrifyingBeam3], new AOEShapeCone(70, 50.Degrees()));
-class SurfaceMissile(BossModule module) : Components.StandardAOEs(module, AID._Weaponskill_SurfaceMissile, 5);
-class PenetratorMissile(BossModule module) : Components.StackWithCastTargets(module, AID._Weaponskill_PenetratorMissile, 5);
-class AntiPersonnelMissile(BossModule module) : Components.SpreadFromCastTargets(module, AID._Weaponskill_AntiPersonnelMissile, 6);
+class EyesOnMe(BossModule module) : Components.RaidwideCast(module, AID.EyesOnMe);
+class PetrifyingBeam(BossModule module) : Components.GroupedAOEs(module, [AID.PetrifyingBeam1, AID.PetrifyingBeam2], new AOEShapeCone(70, 50.Degrees()));
+class SurfaceMissile(BossModule module) : Components.StandardAOEs(module, AID.SurfaceMissile, 5);
+class PenetratorMissile(BossModule module) : Components.StackWithCastTargets(module, AID.PenetratorMissile, 5);
+class AntiPersonnelMissile(BossModule module) : Components.SpreadFromCastTargets(module, AID.AntiPersonnelMissile, 6);
 class MotionScanner(BossModule module) : Components.StayMove(module)
 {
     public override void OnStatusGain(Actor actor, ActorStatus status)
     {
-        if ((SID)status.ID == SID._Gen_MotionTracker && Raid.TryFindSlot(actor, out var slot))
+        if ((SID)status.ID == SID.MotionTracker && Raid.TryFindSlot(actor, out var slot))
             SetState(slot, new(Requirement.Stay, WorldState.CurrentTime));
     }
 
     public override void OnStatusLose(Actor actor, ActorStatus status)
     {
-        if ((SID)status.ID == SID._Gen_MotionTracker && Raid.TryFindSlot(actor, out var slot))
+        if ((SID)status.ID == SID.MotionTracker && Raid.TryFindSlot(actor, out var slot))
             ClearState(slot);
     }
 }
@@ -72,5 +69,5 @@ class D131EyeOfTheScorpionStates : StateMachineBuilder
     }
 }
 
-[ModuleInfo(BossModuleInfo.Maturity.WIP, GroupType = BossModuleInfo.GroupType.CFC, GroupID = 1011, NameID = 14716)]
+[ModuleInfo(BossModuleInfo.Maturity.Verified, GroupType = BossModuleInfo.GroupType.CFC, GroupID = 1011, NameID = 14716)]
 public class D131EyeOfTheScorpion(WorldState ws, Actor primary) : BossModule(ws, primary, new(-615, 575), new ArenaBoundsSquare(20));
